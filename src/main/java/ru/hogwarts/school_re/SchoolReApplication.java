dec7ad1929f572a6fd0534aeffe4fd11e416b1d3
andrehsym <kochetkovandre1993@gmail.com>
2022-02-19 20:06:46 +0300

    1 commit

---
+++ src/main/java/ru/hogwarts/school_re/SchoolReApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/school_re/SchoolReApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
